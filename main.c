38b18cf5c9723dddf1a4475ef5cd5a734aad004e
George Tzalas <it2022102@hua.gr>
2024-12-15 16:58:44 +0200

    Basic structure and input checks

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+bool fileExists(const char* fileName)
+{
+	struct stat st;
+
+	if(stat(fileName, &st) < 0)
+		return false;
+
+	return true;
+}
+
+//Create Cn process with fork
+
+int main(int argc, char* argv[])
+{
+	if(argc != 3)
+	{
+		fprintf(stderr, "Usage: ./rpc [file name] [process count]");
+		return -1;
+	}
+
+	char *fileName = NULL;
+	int processCount = 0;
+
+	if(fileExists(argv[1]))
+	{
+		int nameLength = strlen(argv[1]);
+		fileName = (char*)malloc(sizeof(char) * (nameLength + 1));
+		strcpy(fileName, argv[1]);
+		fileName[nameLength] = 0;
+	}
+	else
+	{
+		fprintf(stderr, "%s", strerror(errno));
+		free(fileName);
+		return -1;
+	}
+
+	if((processCount = atoi(argv[2])) == 0)
+	{
+		fprintf(stderr, "Not a number\n");
+		return -1;
+	}
+
+	free(fileName);	
+	return 0;
+}
